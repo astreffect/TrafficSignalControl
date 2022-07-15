6f53da0f6f97b0b1de3d84200d608e73bba6d8b8
Debarun Acharya <debarunacharya2002@gmail.com>
2022-07-15 07:55:26 +0530

    Debarun's Update-3

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+from src.Simulator import Simulator
+
+if __name__ == "__main__":
+    simulator = Simulator('Fuzzy Traffic System')
+    simulator.start()
+
+
 #To Be Done #Debarun Acharya
 #Disha Panda #Debarun Acharya #2
